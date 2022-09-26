3294e6ce55868b930710480a3140821230b4dd3a
Dave Fisher <dave.j.fish@gmail.com>
2022-09-26 12:56:12 -0700

    added throw away test

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,4 +4,9 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-expect(1).toEqual(1);
+test('expect 1 to be 1', () => {
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  expect(1).toEqual(1);
+});
